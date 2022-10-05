4c9ff6dbe2373994f2d1587b7b479fe877aa5db8
MatthiasCominelli <matticomi12@gmail.com>
2022-10-05 14:28:11 +0200

    migliorato il documento

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
-var nome = prompt ("inserisci il tuo nome")
-var cognome = prompt ("inserisci il tuo cognome")
-var colore = prompt ("inserisci il tuo colore preferito")
-var età = prompt ("inserisci la tua età")
+var nome = prompt ("inserisci il tuo nome");
+var cognome = prompt ("inserisci il tuo cognome");
+var colore = prompt ("inserisci il tuo colore preferito");
+var età = prompt ("inserisci la tua età");
+var password = document.getElementById("autopassword");
 
-document.body.innerHTML = nome + cognome + colore + età
+password.innerHTML = nome + cognome + colore + età 
