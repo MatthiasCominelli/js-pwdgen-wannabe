961b5865756b2782c6e78b7454b44122b03167d1
MatthiasCominelli <matticomi12@gmail.com>
2022-10-05 14:27:29 +0200

    Lavoro Fatto

---
+++ js/script.js
@@ -0,0 +1,6 @@
+var nome = prompt ("inserisci il tuo nome")
+var cognome = prompt ("inserisci il tuo cognome")
+var colore = prompt ("inserisci il tuo colore preferito")
+var età = prompt ("inserisci la tua età")
+
+document.body.innerHTML = nome + cognome + colore + età
